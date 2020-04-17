aad073f4ebf7020d2236d939f4a4f683fad37d16
Muemmelmoehre <muemmelmoehre@protonmail.ch>
2020-04-17 14:08:39 -0400

    adding initial version

---
+++ imageBase64Encoder.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+image=$1
+outfile=$2
+format=$3
+
+if (("$#" < 3)) 
+then
+	echo "Usage : ./imageBase64Encoder.sh <image> <outfile> <image format>" 
+	echo "ex.: ./imageBase64Encoder.sh image.jpeg out.html jpeg"
+else
+	imageBase64=$(base64 $image)
+	echo "<img src=\"data:image/$format;base64,"$imageBase64"\">" > $outfile
+fi
